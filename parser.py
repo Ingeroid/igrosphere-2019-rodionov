f37ffc40a32f294f88389ee9305537435ef9ee8b
Ingeroid <48728914+Ingeroid@users.noreply.github.com>
2019-03-19 23:26:11 +0300

    On branch sergey-rodionov  Changes to be committed: 	modified:   parser.py

--- parser.py
+++ parser.py
@@ -6,7 +6,14 @@ def read_file(file_path):
         return f.read()
 
 
-
+def filter_lines(regexp, lines):
+    result = []
+    for line in lines:
+        if re.match(regexp, line):
+            result.append(line)
+        else:
+            print(f"line {line} don't match")
+    return [line for line in lines if re.match(regexp, line)]
 
 
 if __name__ == "__main__":
