369c1f33f3abfb9c0c620b051ceaf519a9cd4e09
Ingeroid <48728914+Ingeroid@users.noreply.github.com>
2019-03-19 23:18:38 +0300

    Changes to be committed: 	new file:   coverage-error.log 	new file:   parser.py

---
+++ parser.py
@@ -0,0 +1,25 @@
+import re
+
+
+def read_file(file_path):
+    with open(file_path, 'r')as f:
+        return f.read()
+
+
+def filter_lines(regexp, lines):
+    result = []
+    for line in lines:
+        if re.match(regexp, line):
+            result.append(line)
+        else:
+            print(f"line {line} don't match")
+    return [line for line in lines if re.match(regexp, line)]
+
+
+if __name__ == "__main__":
+    filename = "coverage-error.log"
+    regexp = r"\[\d\d\d\d\.\d\d\.\d\d \d\d:\d\d:\d\d\].+"
+    lines = read_file(filename)
+    print("Author is dmitriy.galamaga")
+    print(filter_lines(regexp, lines))
+
